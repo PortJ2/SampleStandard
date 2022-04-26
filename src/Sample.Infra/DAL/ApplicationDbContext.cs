d1fcf5689519fa725dcf6c2a898741b06dbbd572
yun <yun@portj2.com>
2022-04-25 19:07:31 -0700

    Add ApplicationDbContext

---
+++ src/Sample.Infra/DAL/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sample.Infra.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
