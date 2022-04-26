41eb8b872b64329b77b50cada3b5ba94b818b8d8
yun <yun@portj2.com>
2022-04-25 19:06:51 -0700

    Add Email service

---
+++ src/Sample.Core/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace Sample.Core.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+        Task SendEmailAsync(string[] toEmail, string subject, string body);
+    }
+}
