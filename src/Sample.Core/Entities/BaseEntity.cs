aaf1e0392dbb7321451e49eaf9f86f9b1ce2adcc
yun <yun@portj2.com>
2022-04-25 19:07:18 -0700

    Add BaseEntity

---
+++ src/Sample.Core/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Sample.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime CreateDate => DateTime.Now;
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
